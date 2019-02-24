7f9655157d7a8950dbb963e6f1f233040a04d8dd
A Belgrave <abelgrave95@gmail.com>
2019-02-24 18:26:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,11 +2,11 @@
 
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
+  # This and all the other methods take an argument of an integer
   # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
+  # How can we make sure the loop breaks once it has puts out the phrase the
   #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
+  # Maybe we should keep count of the number of times we've puts out the
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
@@ -16,7 +16,7 @@ def loop_iterator(number_of_times)
     puts phrase
     break if counter >= number_of_times
   end
-  
+
 end
 
 def times_iterator(number_of_times)
@@ -26,7 +26,7 @@ def times_iterator(number_of_times)
   number_of_times.times do
     puts phrase
   end
-  
+
 end
 
 def while_iterator(number_of_times)
@@ -39,7 +39,7 @@ def while_iterator(number_of_times)
     counter += 1
     puts phrase
   end
-  
+
 end
 
 def until_iterator(number_of_times)
@@ -58,12 +58,11 @@ end
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
+
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
   for item in 1..number_of_times do
     puts phrase
   end
-  
-end
 
+end
